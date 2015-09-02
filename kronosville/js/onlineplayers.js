3d26ebc1813b7b5a101be055804dec93abdc8823
1b8 <kronosville@gmail.com>
2015-09-02 16:35:12 +1200

    Clean up

--- kronosville/js/onlineplayers.js
+++
@@ -1,8 +0,0 @@
-var playerList = $("#players");
-var avatarWidth = 100;
-
-$.getJSON("http://mcping.net/api/mc.kronosville.net", function(data) {
-	for(var i = 0; i < data.sample.length; i++) {
-		var player = document.createElement("DIV");
-	}
-});
