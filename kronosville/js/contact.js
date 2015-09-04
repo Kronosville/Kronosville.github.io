8c6279d9b40cdf9783ba594105b2e57adbf44a5e
1b8 <kronosville@gmail.com>
2015-09-05 10:15:30 +1200

    Add list of donor 'perks'

--- kronosville/js/contact.js
+++ kronosville/js/contact.js
@@ -1,24 +1,28 @@
 var bodyEl = $("#email-form");
 
+var from = $("#from-field");
+var message = $("#message-field");
+
 function echo(msg) {
 	bodyEl.innerHTML = msg;
 }
 
 function sendEmail() {
-	var from = $("#from-field").value;
-	var message = $("#message-field").value;
+	
 	
 	var error = false;
 	$.ajax("../php/contact.php", {
 		type: "POST",
 		
 		data: {
-			from: from,
-			message: message
+			from: from.value,
+			message: message.value
 		},
 		
 		success: function(sent) {
 			if (sent == true) {
+				from.value = '';
+				message.value = '';
 			} else {error = true;}
 		},
 		error: function() {error = true;}
