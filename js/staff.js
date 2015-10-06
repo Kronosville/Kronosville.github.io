b86b819d091ad5910a09b9f148c5acb86811d8b2
1b8 <kronosville@gmail.com>
2015-10-06 14:35:54 +1300

    Minify files, rewrite /staff to use data from JSON

--- js/staff.js
+++ js/staff.js
@@ -1,49 +1,26 @@
-var por = {
-  ownerEnd: "&4O&cw&6n&ee&ar&8]&4~",
-  mod: "&9[&6Mod&9]&b",
-  ninniy: "Ninniy",
-  drWily: "DrWily",
-  otterP: "OtterP"
-};
+// NOTE Remember to update staff.min.js after editing, using jscompress.com
 
-var staff = [
-  {
-    name: por.ninniy,
-    uuid: "03b187c5f22642469af2a7952f652d31",
-    nick: "&8[" + por.ownerEnd + "&7" + por.ninniy,
-    desc: "The owner of the server. An intergalactic sea slug. \
-    <del>She</del> It likes rice."
-  },
-  {
-    name: "1b8",
-    uuid: "34eb9e87564d44a185918608ab491376",
-    nick: "&8[&6Co&8-" + por.ownerEnd + "D&ar_Po&ek&ae",
-    desc: ""
-  },
-  {
-    name: "DrWily",
-    uuid: "bf23c66fc64c42c9893f785991a3956f",
-    nick: por.mod + por.drWily,
-    desc: ""
-  },
-  {
-    name: "Cpl_" + por.otterP + "aws",
-    uuid: "b4325bccf1624874831cec1dac1850c9",
-    nick: por.mod + "&3Captain_&b" + por.otterP + "irate",
-    desc: ""
+function loadStaff(staff, prefixes) {
+  var tBody = $('<tbody></tbody>');
+
+  for (var i = 0; i < staff.length; i++) {
+    var memb = staff[i];
+
+    tBody.append('<tr>\
+<td><img src="https://crafatar.com/renders/body/' + memb.uuid + '?helm&scale=5"></td>\
+<td>\
+<h2>' + memb.name + '</h2>\
+<p>Chat name: ' + aCodesToHTML(prefixes[memb.rank] + (memb.nick === "off" ? memb.name : memb.nick)) + '<br>' + memb.desc + '</p>\
+</td>\
+</tr>');
   }
-]
 
-var table = $("<tbody></tbody>");
+  $('#staff-table').append(tBody);
 
-for (var i = 0; i < staff.length; i++) {
-  var memb = staff[i];
-  table.append('<tr>\
-    <td><img src="https://crafatar.com/renders/body/' + memb.uuid + '?helm&scale=5"></td>\
-    <td>\
-      <h2>' + memb.name + '</h2>\
-      <p>Chat name: ' + aCodesToHTML(memb.nick) + '<br>' + memb.desc + '</p>\
-    </td>\
-  </tr>');
 }
-$("#staff-table").append(table);
+
+$.getJSON('json/staff/staff.json', function(staff) {
+  $.getJSON('json/staff/prefixes.json', function(prefixes) {
+    loadStaff(staff, prefixes);
+  });
+});
