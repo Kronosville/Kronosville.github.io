a2e6687d891932906386a3f734d9c4ad1a0a3308
1b8 <kronosville@gmail.com>
2015-10-22 19:36:37 +1300

    Create working gallery system, with TODO comments!

--- js/gallery.js
+++ js/gallery.js
@@ -1,32 +1,51 @@
-var fadeTime = 500, imgEl = $('#gal-img'),
-i, gallery; // In milliseconds
+/*
+  fadeTime is in milliseconds. It takes fadeTime to fade in,
+  then fadeTime again to fade out.
+  TODO Maybe take half to fade in and half to fade out?
+*/
 
-function setI() {
-  i = ~~(Math.random() * gallery.length);
+function el(id) {
+  return $('#gallery-' + id);
 }
 
-function loop() {
-  l("Executin'!");
+var fadeTime = 500,
+    imgEl = el('img'),
+    gallery,
+    timeout;
 
-  var curr = gallery[i];
+function doTick() {
 
-  img.fadeOut(fadeTime, function() {
-    img.src = curr.url;
-    img.fadeIn(fadeTime, function() {
-                                                                                l("Faded in...");
-      $('#gal-caption').textContent = '"' + curr.caption + '"';
-      setI();
-      setTimeout(loop, 5000); // 5 secs
+  /* TODO:
+
+    * Maybe set the index to a global variable, and make sure that it doesn't
+    get set to what it was before, using recursion.
+
+    * Also, maybe set gallery to have a specific order, randomly evaluated when
+    window.gallery is set, and add "Back" and "Next" buttons in the HTML. Also
+    add actual ids to those buttons.
+
+  */
+
+  var curr = gallery[~~(Math.random() * gallery.length)];
+
+  // Fade out...
+  imgEl.fadeOut(fadeTime, function() {
+    imgEl.attr('src', curr.url)
+
+    // Fade in...
+    imgEl.fadeIn(fadeTime, function() {
+      el('caption').text('"' + curr.caption + '"');
+
+      // If there's already a timeout running, reset it.
+      if (timeout) clearTimeout(timeout);
+      timeout = setTimeout(doTick, 5000); // 5 secs
     });
   });
 }
 
 $.getJSON('json/gallery.json', function (gallery) {
   window.gallery = gallery;
-  l($('.btn'))
+
+  doTick();
+  $('.btn').click(doTick);
 });
-
-
-
-
-function l(msg){console.log('[Gallery]',msg)}
