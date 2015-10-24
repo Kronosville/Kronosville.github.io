e0471820b0cda3a2e6e8187af17bddb96a4451b3
1b8 <kronosville@gmail.com>
2015-10-25 09:08:30 +1300

    Finish gallery system

--- js/gallery.js
+++ js/gallery.js
@@ -1,51 +1,108 @@
 /*
-  fadeTime is in milliseconds. It takes fadeTime to fade in,
-  then fadeTime again to fade out.
-  TODO Maybe take half to fade in and half to fade out?
+  FADE_TIME is in milliseconds. It takes FADE_TIME to fade in,
+  then FADE_TIME again to fade out.
 */
 
-function el(id) {
-  return $('#gallery-' + id);
+var FADE_TIME = 500,
+
+gallery = [],
+i = 0,
+
+timeout,
+playing = true,
+
+// Only the HTML elements and classes that will be used more than once...
+
+classes = {
+  pre: {
+    GLYPH: 'glyphicon-'
+  }
+  // Fill the rest in after the declaration
+},
+
+els = {
+  SLIDE: $('#img'),
+  PLAY_PAUSE: $('#play-pause')
+};
+
+classes.pre.CHEV = classes.pre.GLYPH + 'chevron-';
+classes.PLAY = classes.pre.GLYPH + 'play';
+classes.PAUSE = classes.pre.GLYPH + 'pause';
+
+function pause() {
+  clearTimeout(timeout);
 }
 
-var fadeTime = 500,
-    imgEl = el('img'),
-    gallery,
-    timeout;
-
-function doTick() {
-
-  /* TODO:
-
-    * Maybe set the index to a global variable, and make sure that it doesn't
-    get set to what it was before, using recursion.
-
-    * Also, maybe set gallery to have a specific order, randomly evaluated when
-    window.gallery is set, and add "Back" and "Next" buttons in the HTML. Also
-    add actual ids to those buttons.
-
-  */
-
-  var curr = gallery[~~(Math.random() * gallery.length)];
+function show() {
+  var curr = gallery[i];
 
   // Fade out...
-  imgEl.fadeOut(fadeTime, function() {
-    imgEl.attr('src', curr.url)
+  els.SLIDE.fadeOut(FADE_TIME, function() {
+    els.SLIDE.attr('src', curr.url);
 
     // Fade in...
-    imgEl.fadeIn(fadeTime, function() {
-      el('caption').text('"' + curr.caption + '"');
+    els.SLIDE.fadeIn(FADE_TIME, function() {
+      $('#caption').text('"' + curr.caption + '"');
 
-      // If there's already a timeout running, reset it.
-      if (timeout) clearTimeout(timeout);
-      timeout = setTimeout(doTick, 5000); // 5 secs
+      if (playing) {
+        // If there's already a timeout running, reset it.
+        pause();
+        timeout = setTimeout(next, 5000); // 5 secs
+      }
     });
   });
 }
 
-$.getJSON('json/gallery.json', function (gallery) {
-  window.gallery = gallery;
+function next() {
+  if (i < gallery.length - 1) i++;
+  else i = 0;
+  show();
+}
 
-  doTick();
-  $('.btn').click(doTick);
+$.getJSON('json/gallery.json', function (data) {
+  var randI, last;
+
+  // Shuffle gallery order and stick it in window.gallery
+  for (var j = 0, len = data.length; j < len; j++) {
+
+    randI = ~~(Math.random() * data.length);
+    gallery.push(data[randI]);
+
+    // Now that we've used `data[randI]`, remove it!
+    // Set the current randI to the last item, and delete the last item.
+    last = data.pop();
+
+    // length is now different
+    if (randI < data.length) data[randI] = last;
+  }
+
+  next();
+  $('.' + classes.pre.CHEV + 'right').click(next);
+
+  $('.' + classes.pre.CHEV + 'left').click(function() {
+
+    if (i > 0) i--;
+    else i = gallery.length - 1;
+
+    show();
+  });
+
+  els.PLAY_PAUSE.click(function() {
+    if (playing) {
+      // Pause
+      els.PLAY_PAUSE.removeClass(classes.PAUSE);
+      els.PLAY_PAUSE.addClass(classes.PLAY);
+
+      playing = false;
+      pause();
+
+    } else {
+      // Play
+      els.PLAY_PAUSE.removeClass(classes.PLAY);
+      els.PLAY_PAUSE.addClass(classes.PAUSE);
+
+      playing = true;
+      next();
+    }
+  });
 });
